b791204c5562447d11e1a38d49ec42e19984efac
Romanio0089 <38210134+Romanio0089@users.noreply.github.com>
2022-12-04 12:55:34 +0100

    update main bat

--- SVRP.bat
+++ SVRP.bat
@@ -15,23 +15,21 @@ SET /p "inputfolder=Copy and paste path to folder with patchable files: "
 ECHO.
 SET /p "f2=Enter name of first .sny file (usually has the shortest name) (example: sony.sny): "
 
-START %cd%\patchmod.exe %inputfolder%\%f2%
 ECHO Process 1 started...
-TIMEOUT /t 5
+START /B /WAIT %cd%\patchmod.exe %inputfolder%\%f2%
 ECHO.
 ECHO Process 1 completed. 
 ECHO Please check in explorer if the first .sny file was modified (date modified). If it was modified at the current time, continue.
 PAUSE
 
 CLS
-START cmd /c "%cd%\wimlib-imagex\wimlib-imagex.exe export %inputfolder%\%f2% 1 %inputfolder%\sony.wim "Sony" --ref="%inputfolder%\*.sny""
 ECHO Process 2 started...
 ECHO.
-ECHO If the command prompt immediately closed as it was opened, please try again.
-TIMEOUT /t 75
+START /B /WAIT cmd /c "%cd%\wimlib-imagex\wimlib-imagex.exe export %inputfolder%\%f2% 1 %inputfolder%\sony.wim "Sony" --ref="%inputfolder%\*.sny""
 ECHO.
-ECHO Process completed.
-ECHO If the command promt suddenly closed before the process reached 100 percent completion, please try again.
+ECHO Process 2 completed.
+ECHO.
+ECHO If an error happened during the process, please try again.
 ECHO.
 ECHO The WIM file should now be in the folder with patchable files!
 ECHO For more information on how to install this WIM file, please follow the documentation on the SVRP GitHub repository.
