6c2f12cc7992d2a44a45fc6006a9bd3a070090d6
Romanio0089 <38210134+Romanio0089@users.noreply.github.com>
2022-12-06 17:31:49 +0100

    update v1.2

--- SVRP.bat
+++ SVRP.bat
@@ -1,22 +1,26 @@
 ECHO OFF & COLOR 17
 CLS & TITLE Sony VAIO Recovery Patcher (SVRP)
 
-ECHO Sony VAIO Recovery Patcher (SVRP) - Batch v1.1
+:MENU
+CLS
+ECHO Sony VAIO Recovery Patcher (SVRP) - Batch v1.2
 ECHO 2022 - Vir0z4 Network IT
 ECHO.
-ECHO This tool supports .sny and .mod files ONLY (for now)!
+ECHO Please read the documentation available on the GitHub before using SVRP! 
 ECHO.
-ECHO Make sure the folder with patchable files is writable, path contains NO spaces, and contains only the .sny or .mod files you want to patch!
+ECHO Make sure the folder with patchable files is writable, path contains no spaces, and contains only the .sny or .mod files you want to patch!
 ECHO.
 
-:MENU
 ECHO Select file extention
 ECHO 1. .sny (Windows installation) (convertion to WIM)
-ECHO 2. .mod (applications) (extracts to folder) (not needed for Windows install)
+ECHO 2. .mod (applications) (extracts to folder)
 ECHO.
-SET /P M=Type 1 or 2 and press ENTER: 
+ECHO 3. Exit
+ECHO.
+SET /P M=Type 1, 2 or 3 and press ENTER: 
 IF %M%==1 GOTO SNY
 IF %M%==2 GOTO MOD
+IF %M%==3 EXIT /B
 IF %INPUT%==false EXIT /B
 
 :SNY
@@ -47,11 +51,11 @@ ECHO.
 ECHO If an error happened during the process, please try again.
 ECHO.
 ECHO The WIM file should now be in the folder with patchable files!
-ECHO For more information on how to install this WIM file, please follow the documentation on the SVRP GitHub repository.
+ECHO Please follow the documentation available on the SVRP GitHub to know how to install Windows from this WIM.
 ECHO.
 
 PAUSE
-EXIT /B
+GOTO MENU
 
 :MOD
 CLS
@@ -74,14 +78,15 @@ PAUSE
 CLS
 ECHO Process 2 started...
 ECHO.
-START /B /WAIT cmd /c "%cd%\wimlib-imagex\wimlib-imagex.exe extract %inputfoldermod%\%m2% 1 --dest-dir=%inputfoldermod%\extract"
+START /B /WAIT cmd /c "mkdir %inputfoldermod%\extract"
+START /B /WAIT cmd /c "%cd%\wimlib-imagex\wimlib-imagex.exe extract %inputfoldermod%\%m2% 1 --dest-dir=%inputfoldermod%\extract\%m2%"
 ECHO.
 ECHO Process 2 completed.
 ECHO.
 ECHO If an error happened during the process, please try again.
 ECHO.
-ECHO The extracted .mod should now be in the "extract" folder in the input folder (folder with patchable files).
+ECHO The extracted .mod should now be in the folder "extract" in the folder with patchable files.
 ECHO.
 
 PAUSE
-EXIT /B
+GOTO MENU
