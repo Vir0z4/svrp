a34122e057c3cf0ee27d37964b26d2b408c180b0
Romanio0089 <38210134+Romanio0089@users.noreply.github.com>
2023-01-29 13:09:37 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,18 +45,6 @@ language = "en"
 html_static_path = ["_static"]
 html_css_files = ["pied-piper-admonition.css"]
 
-html_sidebars = {'**': ['localtoc.html',
-                       ['Getting started',
-                        '- How to find .sny and .mod files <How-to-find-sny-and-mod-files.html>',
-                        'Using SVRP',
-                        '- How to use SVRP <How-to-use-SVRP-GUI.html>',
-                        'Installation',
-                        '- How to install a WIM file <How-to-install-WIM-files.html>',
-                        '- Post-installation instructions <Post-installation.html>',
-                        'Troubleshooting',
-                        '- Troubleshooting <Troubleshooting.html>',
-                        '- BSOD 0x0000012a <BSOD_0x0000012a.html>']]}
-
 html_theme_options: Dict[str, Any] = {
     "footer_icons": [
         {
@@ -125,3 +113,5 @@ if FONT_AWESOME_TESTING:
             "class": "fa-solid fa-github fa-2x",
         },
     ]
+
+html_sidebars = {'**': ['globaltoc.html', '_sidebar.rst']}
